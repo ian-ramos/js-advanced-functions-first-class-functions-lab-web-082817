4ee09df3638b7998c76a0bdf362dda78766e9972
ian-ramos <ramos.ianc@gmail.com>
2017-10-07 19:52:37 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+
+const returnFirstTwoDrivers = function returnFirstTwoDrivers(array) {
+  return array.slice(0, 2);
+}
+
+const returnLastTwoDrivers = function returnLastTwoDrivers(array) {
+  return array.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(multiplier) {
+  return function (fare) {
+    return multiplier * fare
+  }
+}
+
+const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(array, someFunction) { //if you call this function, you need to pass it in the constant, not the function (selectDifferentDrivers(drivers, returnFirstTwoDrivers)).  You can't pass the actual function as the argument 
+  return someFunction(array)
+}
